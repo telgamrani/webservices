54319150c280ce9fa857726d3af34b24ac4342c5
taoufik <taoufik.elgamrani@gmail.com>
2018-05-21 23:35:10 +0200

    create project

---
+++ webservices/src/main/java/com/htw/webservices/WebservicesApplication.java
@@ -0,0 +1,12 @@
+package com.htw.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesApplication.class, args);
+	}
+}
