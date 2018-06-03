b3d95735b09b1220c25eef28de15cd548a2dff52
taoufik <taoufik.elgamrani@gmail.com>
2018-06-03 13:23:45 +0200

    chore(htwWebservices) : using Look model from htwDao

--- htwWebservices/src/main/java/com/htw/webservices/web/controller/LookController.java
+++ htwWebservices/src/main/java/com/htw/webservices/web/controller/LookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.htw.webservices.model.Look;
+import com.htw.dao.model.Look;
 
 @RestController
 public class LookController {
